eb95d4ac4fc4c752a7244d1c07d7139d212c518c
Sandy97 <sandy97@mail.ru>
2016-07-08 16:40:21 +0300

    first commit

---
+++ Unit1.pas
@@ -0,0 +1,64 @@
+unit Unit1;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Ani,
+  FMX.Objects;
+
+type
+  TForm1 = class(TForm)
+    Circle1: TCircle;
+    faRotation: TFloatAnimation;
+    Rectangle2: TRectangle;
+    faY0: TFloatAnimation;
+    faX2: TFloatAnimation;
+    faX1: TFloatAnimation;
+    faY1: TFloatAnimation;
+    faY2: TFloatAnimation;
+    Circle2: TCircle;
+    procedure faY0Finish(Sender: TObject);
+    procedure faY1Finish(Sender: TObject);
+    procedure faY2Finish(Sender: TObject);
+    procedure faX1Finish(Sender: TObject);
+    procedure faX2Finish(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+procedure TForm1.faX1Finish(Sender: TObject);
+begin
+  faX2.Start;
+end;
+
+procedure TForm1.faX2Finish(Sender: TObject);
+begin
+  faX1.Start;
+end;
+
+procedure TForm1.faY0Finish(Sender: TObject);
+begin
+  faY1.Start;
+end;
+
+procedure TForm1.faY1Finish(Sender: TObject);
+begin
+  faY2.Start;
+end;
+
+procedure TForm1.faY2Finish(Sender: TObject);
+begin
+  faY1.Start;
+end;
+
+end.
